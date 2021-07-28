cffc71922bf8cc6ee7195e27350a567d81cdbfaf
Soohyeon Park <sooh21@sookmyung.ac.kr>
2021-07-28 23:17:54 +0900

    2단계 2번 #9498

---
+++ 2_2_b4_9498.py
@@ -0,0 +1,35 @@
+# 9498
+
+# using division
+a = int(input())
+if ((int(a / 10) == 9) or ((a / 10) == 10)):
+    print('A')
+    print(a/10)
+elif (int(a / 10) == 8):
+    print('B')
+elif (int(a / 10) == 7):
+    print('C')
+elif (int(a / 10) == 6):
+    print('D')
+else:
+    print('F')
+
+# using switch --> no switch/case at pythonㅎㅎ
+##a = int(input())
+##switch (a / 10) {
+##    case 10:
+##        print('A')
+##        break
+##    case 9:
+##        print('A')
+##        break
+##    case 8:
+##        print('B')
+##        break
+##    case 7:
+##        print('C')
+##    case 6:
+##        print('D')
+##    default:
+##        print('F')
+##    }
