49cba7dde29ba58b000028671b44c79e739afeb1
Soohyeon Park <sooh21@sookmyung.ac.kr>
2021-08-03 22:35:11 +0900

    3번 #8393

---
+++ 3_3_b5_8393.py
@@ -0,0 +1,19 @@
+# 8393
+
+# 두 풀이 모두 맞음. 그냥 더하기 vs 식 이용 차이.
+
+
+n = int(input())
+sum = 0
+
+for i in range(1, n+1):
+    sum += i
+print(sum)
+
+
+
+
+##n = int(input())
+##sum = (n * (n + 1)) / 2
+##
+##print(int(sum))
