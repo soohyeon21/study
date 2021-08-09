02b7ea5f8012ed31b161d52748bfd38d677921c6
Soohyeon Park <sooh21@sookmyung.ac.kr>
2021-08-09 17:18:19 +0900

    8단계 기본수학1 2번 #2292

---
+++ 8_2_b2_2292.py
@@ -0,0 +1,28 @@
+# 2292
+
+
+# 베껴온 코드
+##n = int(input())
+##
+##nums_pileup = 1  # 벌집의 개수, 1개부터 시작
+##cnt = 1
+##while n > nums_pileup :
+##    nums_pileup += 6 * cnt  # 벌집이 6의 배수로 증가
+##    cnt += 1  # 반복문을 반복하는 횟수
+##print(cnt)
+
+# 일반항 구해서 내가 직접 생각한 코드
+n = int(input())
+r = 1
+more = True # 계속 작업하시오
+
+if (n == r):
+    print(r)
+else:
+    r += 1
+    while (more):
+        if (((3*(r - 1)**2 - 3 * (r - 1) + 1) < n) and (n <= (3*r**2 - 3*r +1))):
+            print(r)
+            more = False
+        else:
+            r += 1
