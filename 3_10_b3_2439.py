877c5278f7ca3618bc81a42e63c4b9dd764db26e
Soohyeon Park <sooh21@sookmyung.ac.kr>
2021-08-09 16:00:29 +0900

    10번 #2439

--- 3_10_b3_2439.py
+++ 3_10_b3_2439.py
@@ -8,6 +8,11 @@ n = int(input())
 for i in range(1, (n + 1)):
     a = "*" * i
     print(a.rjust(n))
+
+
+##n = int(input())
+##
+##for i in range(1, (n + 1)):
 ##    a = "*" * i
 ##    print(("*" * i).rjust(n))
 
