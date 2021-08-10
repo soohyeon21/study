d83a425c3c3812ca833d97a08ebdf04f7f193342
Soohyeon Park <sooh21@sookmyung.ac.kr>
2021-08-10 23:48:59 +0900

    3번 #1193

--- BaekJoon/step8_math1/8_3_b2_1193.py
+++ BaekJoon/step8_math1/8_3_b2_1193.py
@@ -1,5 +1,29 @@
 # 1193
 
+# 대각선으로 1, 2, 3, 4, 5, ... 순으로 갯수가 증가한다.
+# th가 even/odd일 때로 경우의 수가 나눠진다.
+# th는 몇 번째 줄인지 알려주는 variable
+
+# 규칙을 찾자!!
+# 몇 개 해보자.
+
+x = int(input())
+th = 1
+
+while (x > th):
+    x -= th
+    th += 1
+
+if (th % 2 == 0): # th even
+    b = x
+    a = (th + 1) - b
+else: # th odd
+    b = (th + 1) - x
+    a = x
+    
+print("%d/%d" %(b, a))
+
+
 
 # 맞는 답이긴 하지만, 조금 더 간단하게!!!!
 ##x = int(input())
@@ -26,19 +50,3 @@
 ##        b = x
 ##        a = (th + 2) - b
 ##print("%d/%d" %(b, a))
-
-x = int(input())
-th = 1
-
-while (x > th):
-    x -= th
-    th += 1
-
-if (th % 2 == 0): # th even
-    b = x
-    a = (th + 1) - b
-else: # th odd
-    b = (th + 1) - x
-    a = x
-    
-print("%d/%d" %(b, a))
