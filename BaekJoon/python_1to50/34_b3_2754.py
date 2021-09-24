ed5fd576d1692d6d95e877003bf76f53ab3f0eb7
Soohyeon Park <sooh21@sookmyung.ac.kr>
2021-09-24 22:34:04 +0900

    34번 #2754

---
+++ BaekJoon/python_1to50/34_b3_2754.py
@@ -0,0 +1,34 @@
+# 2754
+
+# dictionary를 사용하는 방법도 있다.
+# key-value로 하고, dic[key]를 print해서 value 값 print하는 방식으로
+# if-elif-else는 너무 노가다인듯한 느낌...
+
+c = input()
+
+if (c == 'A+'):
+    print(4.3)
+elif (c == 'A0'):
+    print(4.0)
+elif (c == 'A-'):
+    print(3.7)
+elif (c == 'B+'):
+    print(3.3)
+elif (c == 'B0'):
+    print(3.0)
+elif (c == 'B-'):
+    print(2.7)
+elif (c == 'C+'):
+    print(2.3)
+elif (c == 'C0'):
+    print(2.0)
+elif (c == 'C-'):
+    print(1.7)
+elif (c == 'D+'):
+    print(1.3)
+elif (c == 'D0'):
+    print(1.0)
+elif (c == 'D-'):
+    print(0.7)
+else: # F
+    print(0.0)
