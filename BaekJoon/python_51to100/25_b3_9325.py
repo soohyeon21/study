5eb047b1a630fbd48b172e680b75adb5df3d9ea5
Soohyeon Park <sooh21@sookmyung.ac.kr>
2021-10-19 10:36:56 +0900

    25번 #9325

---
+++ BaekJoon/python_51to100/25_b3_9325.py
@@ -0,0 +1,13 @@
+# 9325
+
+# for문 2개 오랜만이다
+
+t = int(input())
+
+for _ in range(t):
+    s = int(input())
+    n = int(input())
+    for i in range(n):
+        q, p = map(int, input().split())
+        s += q * p
+    print(s)
