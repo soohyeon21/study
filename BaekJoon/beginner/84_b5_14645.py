3f8732c0f462fa033409cf25969c25e0a521ca80
Soohyeon Park <sooh21@sookmyung.ac.kr>
2022-09-23 14:13:22 +0900

    84번 #14645

---
+++ BaekJoon/beginner/84_b5_14645.py
@@ -0,0 +1,12 @@
+# 14645
+
+# 그냥 곧바로 print("비와이")도 맞는 답안 처리가 되어 있다.
+# 아니, '종착역에 도착했을 때, 버스 운전수의 이름을 출력해라.' 하면
+# n개의 입력이 끝난 이후에 print 되야 되는거 아닌감.
+
+import sys
+
+n, k = map(int, sys.stdin.readline().split())
+for _ in range(n):
+    iin, oout = map(int, sys.stdin.readline().split())
+print("비와이")
