6c5a44a251058fc80bba002cc9ea4b020faab8cf
Soohyeon Park <sooh21@sookmyung.ac.kr>
2022-10-19 12:49:01 +0900

    12번 #2605

--- BaekJoon/cpp_351to400/12_b2_2605.py
+++ BaekJoon/cpp_351to400/12_b2_2605.py
@@ -3,11 +3,11 @@
 import sys
 
 n = int(sys.stdin.readline())
-num = list(map(int, sys.stdin.readline().split()))
-ans = []
+line = list(map(int, sys.stdin.readline().split()))
+eat = []
 
 for i in range(n):
-    ans.insert(i-num[i], i+1)
+    eat.insert(line[i], i+1)
 
-for k in ans:
-    print(k, end=' ')
+eat = eat[::-1]
+print(*eat)
