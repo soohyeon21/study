0624cdba8ea385a8810915c12f266e2ba7c540c5
Soohyeon Park <sooh21@sookmyung.ac.kr>
2022-10-20 23:52:12 +0900

    22번 #2669

---
+++ BaekJoon/cpp_351to400/22_b1_2669.py
@@ -0,0 +1,21 @@
+# 2669
+
+# 크으... 졸업 프로젝트가 떠오르는 코드군.
+# 그나저나 그때 도움 되었던 문제가 뭐였더라... 이거랑 비슷했는데.
+
+# plane = [[0] * 101] * 101 # 문제 생긴다. 아마 값을 복사해서 늘리는 방식인듯.
+
+import sys
+
+plane = [[0 for aa in range(101)] for bb in range(101)]
+for _ in range(4):
+    x1, y1, x2, y2 = map(int, sys.stdin.readline().split())
+    for y in range(y1, y2):
+        for x in range(x1, x2):
+            plane[y][x] = 1
+
+cnt = 0
+for i in range(101):
+    cnt += plane[i].count(1)
+
+print(cnt)
