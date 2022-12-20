453248f56ca5d4a086d6b9a0be1c2a0c31e5e152
Soohyeon Park <sooh21@sookmyung.ac.kr>
2022-12-20 16:14:33 +0900

    #23810

---
+++ BaekJoon/series/9567/23810.py
@@ -0,0 +1,14 @@
+# 23810
+
+import sys
+
+n = int(sys.stdin.readline())
+
+for a1 in range(n):
+    print("@"*n*5)
+for a2 in range(n):
+    print("@"*n)
+for a3 in range(n):
+    print("@"*n*5)
+for a4 in range(2*n):
+    print("@"*n)
