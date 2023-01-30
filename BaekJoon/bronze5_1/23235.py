9154b9b48c6cf7dc26247aa3b33a5054b1cfb416
Soohyeon Park <sooh21@sookmyung.ac.kr>
2023-01-30 17:51:46 +0900

    #23235

---
+++ BaekJoon/bronze5_1/23235.py
@@ -0,0 +1,12 @@
+# 23235
+
+import sys
+
+case = 1
+while (1):
+    n = sys.stdin.readline().rstrip()
+    if (n == "0"):
+        break
+
+    print(f'Case {case}: Sorting... done!')
+    case += 1
