31d719e0c2dadc4c802944dbbae2229e76042248
Soohyeon Park <sooh21@sookmyung.ac.kr>
2023-04-23 10:55:41 +0900

    #9493

--- BaekJoon/beginner3/9493.py
+++ BaekJoon/beginner3/9493.py
@@ -1,5 +1,7 @@
 # 9493
 
+# "초(second)는 반올림하고, 시(hour)는 최소 자리로 나타낸다."
+
 import sys
 
 while (1):
@@ -11,4 +13,6 @@ while (1):
     plane = case[0]/case[2]
     time = round(abs(train - plane)*3600)
 
-    print("%d:%02d:%02d" %(time//3600, (time%3600)//60, time%60))
+    print(f"{time//3600}:{(time%3600)//60:02}:{time%60:02}")
+
+##    print("%d:%02d:%02d" %(time//3600, (time%3600)//60, time%60))
