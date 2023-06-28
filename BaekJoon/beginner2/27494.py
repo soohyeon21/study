bf53e47e0c2370e31cb681f847930137a378e085
Soohyeon Park <sooh21@sookmyung.ac.kr>
2023-06-28 19:23:07 +0900

    #27494

---
+++ BaekJoon/beginner2/27494.py
@@ -0,0 +1,24 @@
+# 27494
+
+# python3 시간초과, pypy3 정답
+
+# python3로 할거면, 2가 2개 이상 있는지, 2 이후에 0이 있는지, 0 이후에 2가 있는지, 2 이후에 3이 있는지 if로 판단하면 가능.
+# 아마 n개 숫자 중에서 가능성 없는건 더 자세히 걸러서 시간을 덜 소모하는 듯.
+
+import sys
+
+n = int(sys.stdin.readline())
+
+cnt = 0
+for num in range(2023, n+1):
+    snum = str(num)
+    try:
+        t1 = snum.index("2")
+        t2 = snum[t1+1:].index("0")
+        t3 = snum[t1+t2+2:].index("2")
+        t4 = snum[t1+t2+t3+3:].index("3")
+    except:
+        continue
+    cnt += 1
+
+print(cnt)
