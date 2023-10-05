43a39d66f75696654fb1e3a63f7d991fd6ebf15c
Soohyeon Park <sooh21@sookmyung.ac.kr>
2023-10-05 23:25:03 +0900

    #1620

--- BaekJoon/intermediate4/1620.py
+++ BaekJoon/intermediate4/1620.py
@@ -6,7 +6,7 @@ n, m = map(int, sys.stdin.readline().split())
 pocketmon = {}
 for i in range(1, n+1):
     name = sys.stdin.readline().rstrip()
-    pocketmon[name] = str(i)
+    pocketmon[name] = str(i) # i도 ok. 어차피 print되는 숫자이므로.
     pocketmon[str(i)] = name
 
 for _ in range(m):
