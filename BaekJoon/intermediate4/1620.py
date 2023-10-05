0601ae004aed3b29b1df00981996815332112c21
Soohyeon Park <sooh21@sookmyung.ac.kr>
2023-10-05 23:21:18 +0900

    #1620

---
+++ BaekJoon/intermediate4/1620.py
@@ -0,0 +1,14 @@
+# 1620
+
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+pocketmon = {}
+for i in range(1, n+1):
+    name = sys.stdin.readline().rstrip()
+    pocketmon[name] = str(i)
+    pocketmon[str(i)] = name
+
+for _ in range(m):
+    what = sys.stdin.readline().rstrip()
+    print(pocketmon[what])
