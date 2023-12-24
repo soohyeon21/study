548784465f79fb6ae73de9b3ee3088764855a892
Soohyeon Park <sooh21@sookmyung.ac.kr>
2023-12-24 11:08:03 +0900

    #2897

---
+++ BaekJoon/intermediate3/2897.py
@@ -0,0 +1,24 @@
+# 2398
+
+# 함수 안에서 만들어진 변수를 지역변수, 함수 밖에서 만들어진 변수를 전역변수
+# 함수 안에서 전역변수 만들기: 지역변수 앞에 global 써주기
+
+import sys
+
+def findCrash(plot):
+    if ("#" in plot): # if ("#" not in plot):으로 줄일 수 있겠다.
+        return
+    else:
+        crash[plot.count("X")] += 1
+
+    return
+
+r, c = map(int, sys.stdin.readline().split())
+park = [sys.stdin.readline().rstrip() for _ in range(r)]
+crash = [0, 0, 0, 0, 0]
+
+for row in range(r-1):
+    for col in range(c-1):
+        findCrash(park[row][col]+park[row][col+1]+park[row+1][col]+park[row+1][col+1])
+
+print(*crash, sep="\n")
