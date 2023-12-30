4b4ae42d8a10fb6b320285911f1511ecb0236323
Soohyeon Park <sooh21@sookmyung.ac.kr>
2023-12-31 00:57:03 +0900

    #16676

--- BaekJoon/intermediate4/16676.py
+++ BaekJoon/intermediate4/16676.py
@@ -1,39 +1,16 @@
 # 16676
 
-#
-# sol1)
-#
 import sys
 
 n = int(sys.stdin.readline())
-sticker = 1
+digit = "1"
+stickers = 0
 
-if (n <= 10):
-    print(1)
+if (n < 10):
+    stickers = 1
 else:
-    while (n >= sticker):
-        sticker = int(str(sticker) + "1")
-    print(len(str(sticker//10)))
+    while (n >= int(digit)):
+        digit = digit+"1"
+    stickers = len(digit) -1
 
-
-
-#
-# sol2)
-#
-##import sys
-##
-##n = int(sys.stdin.readline())
-##
-##lenn = len(str(n))
-##alldig = str(n)[0]*lenn
-##sticker = lenn
-##
-##for i in range(lenn):
-##    if (n <= (int(alldig)//10**i)*10**i):
-##        sticker = lenn-i
-##    else:
-##        break
-##
-##sticker = max(sticker, str(n).count("0"))
-##
-##print(sticker)
+print(stickers)
