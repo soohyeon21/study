02c332b1fa8db2a4f1f521fbd36f3f61b35ce59d
Soohyeon Park <sooh21@sookmyung.ac.kr>
2024-03-18 23:34:35 +0900

    #11576

---
+++ BaekJoon/algorithm_basic1/11576.py
@@ -0,0 +1,24 @@
+# 11576
+
+# or python 진법 변환 module 사용하기?
+
+#
+# sol1) A진법 -> 10진법 -> B진법
+#
+import sys
+
+A, B = map(int, sys.stdin.readline().split())
+m = int(sys.stdin.readline())
+numA = list(map(int, sys.stdin.readline().split()))
+
+num10 = 0
+for i in range(m):
+    num10 += numA[i] * A**(m-1-i)
+
+numB = []
+while (num10 != 0):
+    digit = num10 % B
+    numB.insert(0, digit)
+    num10 //= B
+
+print(*numB)
