782a8626df6900ea2261b28c73a2a6c7162f8329
Soohyeon Park <sooh21@sookmyung.ac.kr>
2024-06-17 19:09:57 +0900

    #17213

---
+++ BaekJoon/intermediate6/17213.py
@@ -0,0 +1,14 @@
+# 17213
+
+# math.comb or math.factorial()
+
+import sys
+
+n = int(sys.stdin.readline())
+m = int(sys.stdin.readline())
+
+fact = [1, 1, 2]
+for i in range(3, 30):
+    fact.append(fact[i-1]*i)
+
+print(fact[m-1]//(fact[n-1]*fact[m-n]))
