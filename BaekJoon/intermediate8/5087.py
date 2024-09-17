7f6affe88f3a750c760a12b4cd8c0cb2cc08956a
Soohyeon Park <sooh21@sookmyung.ac.kr>
2024-09-18 00:04:52 +0900

    #5087

--- BaekJoon/intermediate8/5087.py
+++ BaekJoon/intermediate8/5087.py
@@ -2,3 +2,28 @@
 
 import sys
 
+while (1):
+    *card, star = sys.stdin.readline().split()
+    if (star == "#"):
+        break
+
+    ncard = []
+    for c in card:
+        if (c == "A"):
+            ncard.append(1)
+        else:
+            ncard.append(int(c))
+
+    cheryl, tania = 0, 0
+    for each in ncard:
+        if (each%2 != 0):
+            cheryl += 1
+        else:
+            tania += 1
+
+    if (cheryl > tania):
+        print("Cheryl")
+    elif (cheryl < tania):
+        print("Tania")
+    else:
+        print("Draw")
