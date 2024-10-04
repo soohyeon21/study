79721197c49b3cb52d79ff86ea0ef38e97727ce5
Soohyeon Park <sooh21@sookmyung.ac.kr>
2024-10-04 14:22:25 +0900

    #6974

---
+++ BaekJoon/intermediate8/6974.py
@@ -0,0 +1,11 @@
+# 6974
+
+import sys
+
+n = int(sys.stdin.readline())
+for _ in range(n):
+    num = int(sys.stdin.readline())
+    divi = int(sys.stdin.readline())
+    print(num//divi)
+    print(num%divi)
+    print()
