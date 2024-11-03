f03b461c104d8d2322aaf7595ace6341630b1d82
Soohyeon Park <sooh21@sookmyung.ac.kr>
2024-11-03 23:59:12 +0900

    #18063

---
+++ BaekJoon/intermediate8/18063.py
@@ -0,0 +1,21 @@
+# 18063
+
+import sys
+
+n, c = map(int, sys.stdin.readline().split())
+
+hour, minute, second = 0, 0, 0
+t = []
+for _ in range(n):
+    m, s = map(int, sys.stdin.readline().split(":"))
+    t.append(m*60+s)
+
+second = sum(t)
+for i in range(1, n):
+    second -= min(t[i-1], t[i], c)
+
+minute = (second//60)%60
+second %= 60
+hour = minute//60
+
+print(f"{hour:02}:{minute:02}:{second:02}")
