45cc3bf234bafeefd34e29e333aa180836eace1a
Soohyeon Park <sooh21@sookmyung.ac.kr>
2025-02-05 16:44:51 +0900

    #30404

--- BaekJoon/greedy_basic_c/30404.py
+++ BaekJoon/greedy_basic_c/30404.py
@@ -1,19 +1,41 @@
 # 30404
 
+#
+# sol1) raw
+#
+##import sys
+##
+##n, k = map(int, sys.stdin.readline().split())
+##xi = list(map(int, sys.stdin.readline().split())) + [10**7]
+##
+##pin = 0
+##quack = xi[0]
+##clap = 0
+##while (quack != 10**7):
+##    for i in range(pin, n+1):
+##        if (quack+k < xi[i]):
+##            quack = xi[i]
+##            clap += 1
+##            pin = i
+##            break
+##
+##print(clap)
+
+
+
+#
+# sol2) 정리된 형태
+#
 import sys
 
 n, k = map(int, sys.stdin.readline().split())
-xi = list(map(int, sys.stdin.readline().split())) + [10**7]
+xi = list(map(int, sys.stdin.readline().split()))
 
-pin = 0
-quack = xi[0]
-clap = 0
-while (quack != 10**7):
-    for i in range(pin, n+1):
-        if (quack+k < xi[i]):
-            quack = xi[i]
-            clap += 1
-            pin = i
-            break
+cnt = 1
+num = xi[0] + k
+for i in range(1, n):
+    if (num < xi[i]):
+        cnt += 1
+        num = xi[i]+k
 
-print(clap)
+print(cnt)
