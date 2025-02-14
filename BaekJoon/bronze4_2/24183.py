7c139a145e1387558ad15b3b8dbb6a1d6418c2be
Soohyeon Park <sooh21@sookmyung.ac.kr>
2025-02-14 17:19:13 +0900

    #24183

---
+++ BaekJoon/bronze4_2/24183.py
@@ -0,0 +1,8 @@
+# 24183
+
+import sys
+
+c4, a3, a4 = map(int, sys.stdin.readline().split())
+
+total = ((c4*2 * 229*324*10**-4) + (a3*2 * 297*420*10**-4) + (a4 * 210*297*10**-4))*10**-2
+print(total)
