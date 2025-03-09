4d95953b306bb34b6f7c0b27f124d29c5c6d4ff4
Soohyeon Park <sooh21@sookmyung.ac.kr>
2025-03-09 23:57:00 +0900

    #9324

--- BaekJoon/intermediate5/9324.py
+++ BaekJoon/intermediate5/9324.py
@@ -3,63 +3,28 @@
 import sys
 
 t = int(sys.stdin.readline())
+
 for _ in range(t):
-    msg = sys.stdin.readline().rstrip()
+    m = sys.stdin.readline().rstrip()
 
-    many = {}
-    before = ""
-    for letter in msg:
-        many[letter] = many.setdefault(letter, 0)
-        many[letter] += 1
-        if (many[letter] == 3):
-            many[letter] = 0
-        else:
-            before += letter
+    check = {}
+    now = ""
+    state = True
+    for letter in m:
+        check[letter] = check.setdefault(letter, 0)
+        check[letter] += 1
+        if (check[letter] == 3):
+            now = letter
+        if (check[letter] == 4):
+            if (now != letter):
+                state = False
+                break
+            else:
+                check[letter] = 0
+    if (3 in check.values()):
+        state = False
 
-    many = {}
-    real = ""
-    for letter in before:
-        many[letter] = many.setdefault(letter, 0)
-        many[letter] += 1
-        if (many[letter] == 3):
-            real += letter
-            many[letter] = 0
-        real += letter
-        
-##    object_strs = [msg, before, real]
-##    for i in range(2):# in object_strs:
-##        many = {}
-##        for letter in object_strs[i]:
-##            many[letter] = many.setdefault(letter, 0)
-##            many[letter] += 1
-##            if (i == 0):
-##                print(letter, many[letter])
-##                if (many[letter] == 3):
-##                    many[letter] = 0
-##                else:
-##                    object_strs[i+1] += letter
-##            elif (i == 1):
-##                pass
-##            if (many[letter] == 3):
-##                many[letter] = 0
-##            else:
-##                object_strs[i+1] += letter
-
-    #print(f"msg: {msg}, before: {before}, real:{real}")
-    if (msg == real):
+    if (state):
         print("OK")
     else:
         print("FAKE")
-            
-
-
-        
-##    for letter in msg:
-##        many[letter] = many.setdefault(letter, 0)
-##        many[letter] += 1
-##        if (many[letter] == 3):
-##            many[letter] = 0
-##        else:
-##            before += letter
-##
-##    for letter in before:
