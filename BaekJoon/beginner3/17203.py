f73e60f452e6c94b935bc1a7d7a5b42f99397aa1
Soohyeon Park <sooh21@sookmyung.ac.kr>
2025-03-17 21:24:53 +0900

    #17203

--- BaekJoon/beginner3/17203.py
+++ BaekJoon/beginner3/17203.py
@@ -2,14 +2,40 @@
 
 # 변화량 list, 누적합 list, 단순 value[end]-value[start] 사용하면 시간 1/10 수준으로 대폭 단축 가능.
 
+# 부분 배열 합: O(N)
+# 누적합: O(1)
+
+# 
+# sol1) 매번 델타 구하기
+# 32412 KB, 316 ms
+#
+##import sys
+##
+##n, q = map(int, sys.stdin.readline().split())
+##an = list(map(int, sys.stdin.readline().split()))
+##
+##for _ in range(q):
+##    start, end = map(int, sys.stdin.readline().split())
+##    delta = 0
+##    for i in range(start-1, end-1):
+##        delta += abs(an[i]-an[i+1])
+##    print(delta)
+
+
+
+#
+# sol2) 누적합 구하기
+# 32412 KB, 36 ms
+#
 import sys
 
-n, q = map(int, sys.stdin.readline().split())
+n, m = map(int, sys.stdin.readline().split())
 an = list(map(int, sys.stdin.readline().split()))
 
-for _ in range(q):
+diff_cum = [0 for i in range(n)]
+for j in range(1, n):
+    diff_cum[j] = diff_cum[j-1] + abs(an[j]-an[j-1])
+
+for _ in range(m):
     start, end = map(int, sys.stdin.readline().split())
-    delta = 0
-    for i in range(start-1, end-1):
-        delta += abs(an[i]-an[i+1])
-    print(delta)
+    print(diff_cum[end-1] - diff_cum[start-1])
