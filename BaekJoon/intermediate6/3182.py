0b481e583ece42f9a1e671bfc7de47e918450b62
Soohyeon Park <sooh21@sookmyung.ac.kr>
2025-03-23 23:44:04 +0900

    #3182

--- BaekJoon/intermediate6/3182.py
+++ BaekJoon/intermediate6/3182.py
@@ -1,13 +1,34 @@
 # 3182
 
+# DFS 문제.
+# 단순히 개수 세는 문제는 아니다.
+
 import sys
 
+sys.setrecursionlimit(10**6)
+
+def DFS(node):
+    global cnt
+    
+    visited[node] = True
+    for next_node in graph[node]:
+        if (not visited[next_node]):
+            DFS(next_node)
+    cnt += 1
+
+
 n = int(sys.stdin.readline())
+graph = [[] for _ in range(n+1)]
+for u in range(1, n+1):
+    v = int(sys.stdin.readline())
+    graph[u].append(v)
 
-seni = {i:0 for i in range(1, n+1)}
-for _ in range(n):
-    ask = int(sys.stdin.readline())
-    seni[ask] += 1
+ask = []
+for seni in range(1, n+1):
+    visited = [False for i1 in range(n+1)]
+    cnt = 0
+    DFS(seni)
+    ask.append((seni, cnt))
 
-sort_seni = sorted(list(seni.items()), key=lambda x:(x[1], x[0]))
-print(sort_seni[0][0])
+ask.sort(key=lambda x:(-x[1], x[0]))
+print(ask[0][0])
