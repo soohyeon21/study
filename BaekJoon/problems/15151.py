e776efae2dc2230cee8ffafb24a34a204fdf1e5a
Soohyeon Park <sooh21@sookmyung.ac.kr>
2025-04-06 00:50:39 +0900

    #15151

--- BaekJoon/problems/15151.py
+++ BaekJoon/problems/15151.py
@@ -4,7 +4,10 @@ import sys
 
 k, d = map(int, sys.stdin.readline().split())
 
-D = ((k+1)**2 - 4*(k-2*d))**0.5
-upper = (-k-1+D)/(2*k+2)
+books = 0
+while (d >= k):
+    d -= k
+    books += 1
+    k *= 2
 
-print(int(upper)+1)
+print(books)
