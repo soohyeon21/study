b5fea122c1f9e30191f2bdb0c5c44735e76ce4c6
Soohyeon Park <sooh21@sookmyung.ac.kr>
2025-04-22 23:58:54 +0900

    #1590

--- BaekJoon/problems/1590.py
+++ BaekJoon/problems/1590.py
@@ -3,28 +3,32 @@
 import sys
 
 n, t = map(int, sys.stdin.readline().split())
-bus = []
+
+result = []
 for _ in range(n):
     s, l, c = map(int, sys.stdin.readline().split())
     bus += [s+i*l for i in range(c)]
 
-bus.sort()
-wait = 0
+    if (bus[-1] < t):
+        continue
 
-if (bus[-1] < t):
-    wait = -1
-else:
-    left = 0
-    right = len(bus)-1
-    answer = 0
-    while (left <= right):
-        #print(f"left={left}, right={right}")
-        mid = (left+right)//2
-        if (bus[mid] < t):
-            answer = mid
-            left = mid + 1
-        else:
-            right = mid - 1
+    bus.sort()
+    wait = 0
+
+    if (bus[-1] < t):
+        wait = -1
+    else:
+        left = 0
+        right = len(bus)-1
+        answer = 0
+        while (left <= right):
+            #print(f"left={left}, right={right}")
+            mid = (left+right)//2
+            if (bus[mid] < t):
+                answer = mid
+                left = mid + 1
+            else:
+                right = mid - 1
             
             
 ##        if (t < bus[(left+right)//2]):
