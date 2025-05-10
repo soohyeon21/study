f9b03dfffe024f229d6268c84c0dd67b8ce2421a
Soohyeon Park <sooh21@sookmyung.ac.kr>
2025-05-10 22:27:32 +0900

    #26766

---
+++ BaekJoon/problems/26766.py
@@ -0,0 +1,9 @@
+# 26766
+
+import sys
+
+n = int(sys.stdin.readline())
+
+heart = " @@@   @@@ \n@   @ @   @\n@    @    @\n@         @\n @       @ \n  @     @  \n   @   @   \n    @ @    \n     @     "
+for _ in range(n):
+    print(heart)
