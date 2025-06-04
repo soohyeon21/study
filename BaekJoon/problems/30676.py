5cae91927f44a22b198d083c9d8c989b11f96116
Soohyeon Park <sooh21@sookmyung.ac.kr>
2025-06-04 21:23:34 +0900

    #30676

---
+++ BaekJoon/problems/30676.py
@@ -0,0 +1,20 @@
+# 30676
+
+import sys
+
+lamd = int(sys.stdin.readline())
+
+if (620 <= lamd <= 780):
+    print("Red")
+elif (590 <= lamd < 620):
+    print("Orange")
+elif (570 <= lamd < 590):
+    print("Yellow")
+elif (495 <= lamd < 570):
+    print("Green")
+elif (450 <= lamd < 495):
+    print("Blue")
+elif (425 <= lamd < 450):
+    print("Indigo")
+elif (380 <= lamd < 425):
+    print("Violet")
