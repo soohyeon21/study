9347d415857f870dbf1a09c2006e26bd2203a687
Soohyeon Park <sooh21@sookmyung.ac.kr>
2025-07-06 00:02:59 +0900

    #24267

--- BaekJoon/problems/24267.py
+++ BaekJoon/problems/24267.py
@@ -1,14 +1,14 @@
 # 24267
 
+# (n-2)*(n-1)*n / 6
+
 import sys
 
 n = int(sys.stdin.readline())
 
-cnt = 0
+total = 0
 for i in range(1, n-1):
-    for j in range(i+1, n):
-        for k in range(j+1, n+1):
-            cnt += 1
+    total += i*(n-1-i)
 
-print(cnt)
+print(total)
 print(3)
