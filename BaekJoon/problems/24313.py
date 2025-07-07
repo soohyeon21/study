d80aa9f4cb4affab9f6d9e75671b81973ed8ca4e
Soohyeon Park <sooh21@sookmyung.ac.kr>
2025-07-07 13:33:05 +0900

    #24313

--- BaekJoon/problems/24313.py
+++ BaekJoon/problems/24313.py
@@ -1,5 +1,8 @@
 # 24313
 
+# (a1*n0+a0 <= c*n0) and (c-a1>=0) # 이렇게만 해도 충분
+# (c-a1<0)이면 '모든 n>=n0'에 대해서 성립이 안된다.
+
 import sys
 
 a1, a0 = map(int, sys.stdin.readline().split())
@@ -11,9 +14,8 @@ if ((c-a1) > 0):
     if (a0/(c-a1) > n0):
         state = False
 elif ((c-a1) < 0):
-    if (a0/(c-a1) < n0):
-        state = False
-elif (c-a1 == 0):
+    state = False
+elif (c == a1):
     if (a0 > 0):
         state = False
 
