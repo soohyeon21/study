8730118b8108f947bb2f2f2d8c7d0517c6959f6b
Soohyeon Park <sooh21@sookmyung.ac.kr>
2025-08-23 23:17:31 +0900

    #5344

---
+++ BaekJoon/problems/5344.py
@@ -0,0 +1,17 @@
+# 5344
+
+import sys
+
+def gcf(a, b):
+    while (b != 0):
+        num = a%b
+        a = b
+        b = num
+    return a
+
+
+n = int(sys.stdin.readline())
+
+for _ in range(n):
+    a, b = map(int, sys.stdin.readline().split())
+    print(gcf(a, b))
