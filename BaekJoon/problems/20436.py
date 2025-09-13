74de9749438ca5bcb2582cdf814f367d16638f7e
Soohyeon Park <sooh21@sookmyung.ac.kr>
2025-09-13 23:59:47 +0900

    #20436

---
+++ BaekJoon/problems/20436.py
@@ -0,0 +1,15 @@
+# 20436
+
+import sys
+
+left = "qwertasdfgzxcv"
+pos = {'q':(0, 0), 'w':(0, 1), 'e':(0, 2), 'r':(0, 3), 't':(0, 4), 'y':(0, 5), 'u':(0, 6), 'i':(0, 7), 'o':(0, 8), 'p':(0, 9),
+       'a':(1, 0)}
+
+sl, sr = sys.stdin.readline().split()
+types = sys.stdin.readline().rstrip()
+
+l_now = 
+for letter in types:
+    if (letter in left):
+        
