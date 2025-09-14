2d482f97f87b5b60d87d3c675b2c5ae0b3d4f3e9
Soohyeon Park <sooh21@sookmyung.ac.kr>
2025-09-14 23:56:29 +0900

    #20436

--- BaekJoon/problems/20436.py
+++ BaekJoon/problems/20436.py
@@ -4,12 +4,23 @@ import sys
 
 left = "qwertasdfgzxcv"
 pos = {'q':(0, 0), 'w':(0, 1), 'e':(0, 2), 'r':(0, 3), 't':(0, 4), 'y':(0, 5), 'u':(0, 6), 'i':(0, 7), 'o':(0, 8), 'p':(0, 9),
-       'a':(1, 0)}
+       'a':(1, 0), 's':(1, 1), 'd':(1, 2), 'f':(1, 3), 'g':(1, 4), 'h':(1, 5), 'j':(1, 6), 'k':(1, 7), 'l':(1, 8),
+       'z':(2, 0), 'x':(2, 1), 'c':(2, 2), 'v':(2, 3), 'b':(2, 4), 'n':(2, 5), 'n':(2, 6), 'm':(2, 7)}
 
 sl, sr = sys.stdin.readline().split()
 types = sys.stdin.readline().rstrip()
 
-l_now = 
+l_now = pos[sl]
+r_now = pos[sr]
+total = 0
 for letter in types:
+    dist = 0
     if (letter in left):
-        
+        dist = abs(l_now[0]-pos[letter][0])+abs(l_now[1]-pos[letter][1])
+        l_now = pos[letter]
+    else:
+        dist = abs(r_now[0]-pos[letter][0])+abs(r_now[1]-pos[letter][1])
+        r_now = pos[letter]
+    total += dist+1
+
+print(total)
