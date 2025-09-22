c2081f0195df728a4ae3f14fed7399ef8901af41
Soohyeon Park <sooh21@sookmyung.ac.kr>
2025-09-22 14:09:36 +0900

    #15465

--- BaekJoon/problems/15465.py
+++ BaekJoon/problems/15465.py
@@ -1,27 +1,35 @@
 # 15465
 
+# 각 기록마다 display 현황을 조사해서, 직전 display와 비교하는 방법.
+
 import sys
 
 n = int(sys.stdin.readline())
 
-milk = {'B':7, 'E':7, 'M':7}
 record = []
 for _ in range(n):
-    day, name, pm = sys.stdin.readline().split()
-    record.append((int(day), name, int(pm)))
+    order, name, delta = sys.stdin.readline().split()
+    record.append((int(order), name, int(delta)))
+
 record.sort()
 
+display_name = ['']
+display_score = 7
 change = 0
-now_max = 7
-now_who = ''
-for one in record:
-    milk[one[1][0]] += one[2]
-    # max값이 변하면 변경o
-    ## max값이 변해도, 동일 인물이면 변경x
-    ### max값이 변했고, 동일 인물인데, 다른 인물이 또 있으면 변경o
-    if ((now_max != max(milk.values())) and ((now_who != one[1][0]) or (now_who == '')) or ((max(milk.values()) == milk[one[1][0]]) and (list(milk.values()).count(milk[one[1][0]]) > 1))):
-        now_who = one[1][0]
+milk = {'Bessie':7, 'Elsie':7, 'Mildred':7}
+for cow in record:
+    new_name = []
+    new_max_score = 0
+    milk[cow[1]] += cow[2]
+
+    new_max_score = max(milk.values())
+    for one in sorted(milk.items(), reverse=True):
+        if (one[1] == new_max_score):
+            new_name.append(one[0])
+
+    if (display_name != new_name):
         change += 1
-        now_max = max(milk.values())
+        display_name = new_name
+        display_score = new_max_score
 
 print(change)
