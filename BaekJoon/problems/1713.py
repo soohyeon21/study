009b5d624438b3fa15b2dc444ed86bb22022dc76
Soohyeon Park <sooh21@sookmyung.ac.kr>
2025-10-05 23:56:33 +0900

    #1713

--- BaekJoon/problems/1713.py
+++ BaekJoon/problems/1713.py
@@ -6,11 +6,16 @@ n = int(sys.stdin.readline())
 rec = int(sys.stdin.readline())
 student = list(map(int, sys.stdin.readline().split()))
 
-frame = [[0, 0, 0] for _ in range(n)] # [[학생번호, 순서, 횟수]]
-for i in range(n):
-    frame[i][0] = student[i]
-    frame[i][1] = i+1
-    frame[i][2] = 1
-
-for k in range(n, rec):
-    pass
+frame = {i:[0, 0, 0] for i in range(1, 101)} # {학생번호:[on/off, 추천횟수, 게시순서]}
+order = 1
+for one in student:
+    occupied = sum(v[0] for v in frame.values())
+    if (occupied < n): # 빈 사진틀이 남아있는 경우
+        frame[one][0] = 1
+        frame[one][1] += 1
+        order = min(order, frame[one][2])
+    else: # 빈 사진틀이 없는 경우
+        if (frame[one][0] == 1): # 이미 게시되어 있는 경우
+            frame[one][1] += 1
+        else: # 삭제가 필요한 경우
+            pass
