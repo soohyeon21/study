e1458dbe3f567be325b5557b5227487ee1094009
Soohyeon Park <sooh21@sookmyung.ac.kr>
2025-10-20 15:14:48 +0900

    #15071

--- BaekJoon/beginner3/15071.py
+++ BaekJoon/beginner3/15071.py
@@ -1,26 +1,23 @@
 # 15071
 
+# ex) msg-key-cip은 ord 상에서 아래와 같은 상관관계가 있다.
+# (msg) SENDMOREMONKEYS : [18, 4, 13, 3, 12, 14, 17, 4, 12, 14, 13, 10, 4, 24, 18]
+# (key) ACMSENDMOREMONK : [0, 2, 12, 18, 4, 13, 3, 12, 14, 17, 4, 12, 14, 13, 10]
+# (cip) SGZVQBUQAFRWSLC : [18, 6, 25, 21, 16, 1, 20, 16, 0, 5, 17, 22, 18, 11, 2]
+
 import sys
 
 cipher = sys.stdin.readline().rstrip()
 word = sys.stdin.readline().rstrip()
 
 msg_ord = []
-for i in range(len(word)):
-    msg_ord.append(((ord(cipher[i])-65) - (ord(word[i])-65)) % 26)
+word_ord = [ord(letter)-65 for letter in word]
 
-word_ord = [ord(letter)-65 for letter in word]+msg_ord[len(word):2*len(word)]
+for i in range(len(cipher)):
+    tmp = ((ord(cipher[i])-65) - word_ord[i]) % 26
+    msg_ord.append(tmp)
+    if (len(word_ord) < len(cipher)):
+        word_ord.append(tmp)
 
-for k in range(len(cipher)//len(word)-1):
-    print(f"k={k}")
-    word_ord += msg_ord[(k)*len(word):(k+1)*len(word)]
-    for i in range((k+1)*len(word), (k+2)*len(word)):
-        msg_ord.append(((ord(cipher[i])-65) - word_ord[i]) % 26)
-        print(f"msg_ord : {msg_ord}")
-        print(f"original: {[18, 4, 13, 3, 12, 14, 17, 4, 12, 14, 13, 10, 4, 24, 18]}")
-        print(f"key_ord : {word_ord}")
-        print(f"original: {[0, 2, 12, 18, 4, 13, 3, 12, 14, 17, 4, 12, 14, 13, 10]}")
-        print(f"cip_ord : {[18, 6, 25, 21, 16, 1, 20, 16, 0, 5, 17, 22, 18, 11, 2]}")
-        print(f"====")
-
-print(msg_ord)
+for each in msg_ord:
+    print(chr(each+65), end='')
