148f6bd3d76a2032871cc38f7a9613f4021af9c6
Soohyeon Park <sooh21@sookmyung.ac.kr>
2025-11-02 17:52:24 +0900

    #8416

--- BaekJoon/problems/8416.py
+++ BaekJoon/problems/8416.py
@@ -1,7 +1,21 @@
 # 8416
 
+# or %10 활용
+
 import sys
 
 a, b = map(int, sys.stdin.readline().split())
 
-print(a**b)
+end = []
+
+one = 1
+for i in range(b):
+    one = int(str(one*a)[-1])
+    if (one not in end):
+        end.append(one)
+    else:
+        break
+
+remain = b%len(end)
+
+print(end[remain-1])
