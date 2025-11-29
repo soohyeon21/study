1b790ffcfe02a748be3a93e97fc0bfca30672617
Soohyeon Park <sooh21@sookmyung.ac.kr>
2025-11-30 00:03:21 +0900

    #13420

--- BaekJoon/problems/13420.py
+++ BaekJoon/problems/13420.py
@@ -2,3 +2,22 @@
 
 import sys
 
+def cal(equ):
+    if (equ[1] == '+'):
+        return int(equ[0]) + int(equ[2])
+    elif (equ[1] == '-'):
+        return int(equ[0]) - int(equ[2])
+    elif (equ[1] == '*'):
+        return int(equ[0]) * int(equ[2])
+    elif (equ[1] == '/'):
+        return int(equ[0]) // int(equ[2])
+
+
+t = int(sys.stdin.readline())
+for _ in range(t):
+    ipt = sys.stdin.readline().split()
+
+    if (cal(ipt) == int(ipt[-1])):
+        print('correct')
+    else:
+        print('wrong answer')
