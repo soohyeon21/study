b75febe3c0bb3a8c435ce5628c3d1cdc407022b2
Soohyeon Park <sooh21@sookmyung.ac.kr>
2026-01-06 23:52:23 +0900

    #34691

---
+++ BaekJoon/problems/34691.py
@@ -0,0 +1,15 @@
+# 34691
+
+import sys
+
+while (1):
+    ipt = sys.stdin.readline().rstrip()
+    if (ipt == 'end'):
+        break
+
+    if (ipt == 'animal'):
+        print('Panthera tigris')
+    elif (ipt == 'tree'):
+        print('Pinus densiflora')
+    elif (ipt == 'flower'):
+        print('Forsythia koreana')
