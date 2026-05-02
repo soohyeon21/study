fe7fceb1e598783415764da0c55334f7ca18a356
Soohyeon Park <sooh21@sookmyung.ac.kr>
2026-05-02 21:16:58 +0900

    #pcce_05_walk

---
+++ programmers/level_0/pcce_05_walk.py
@@ -0,0 +1,15 @@
+# [PCCE 기출문제] 5번 / 산책
+
+def solution(route):
+    east = 0
+    north = 0
+    for i in route:
+        if i == "N":
+            north += 1
+        elif i == "S" :
+            north -= 1
+        elif i == "E" :
+            east += 1
+        elif i == "W" :
+            east -= 1
+    return [east, north]
