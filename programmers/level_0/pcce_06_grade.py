2b846c62da748f2a2b3d83d3e562d1b8d81d4f38
Soohyeon Park <sooh21@sookmyung.ac.kr>
2026-05-03 23:26:16 +0900

    #pcce_06_grade

---
+++ programmers/level_0/pcce_06_grade.py
@@ -0,0 +1,11 @@
+# [PCCE 기출문제] 6번 / 가채점
+
+def solution(numbers, our_score, score_list):
+    answer = []
+    for i in range(len(numbers)):
+        if (our_score[i] == score_list[numbers[i]-1]):
+            answer.append("Same")
+        else:
+            answer.append("Different")
+
+    return answer
