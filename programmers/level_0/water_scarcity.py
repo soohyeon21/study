2036910bc11cd8f8802dc6dae945a5765cdd763e
Soohyeon Park <sooh21@sookmyung.ac.kr>
2026-05-11 23:56:37 +0900

    #water_scarcity

---
+++ programmers/level_0/water_scarcity.py
@@ -0,0 +1,11 @@
+# [PCCE 기출문제] 6번 / 물 부족
+
+def solution(storage, usage, change):
+    total_usage = 0
+    for i in range(len(change)):
+        usage = usage * (1+change[i]/100)
+        total_usage += usage
+        if total_usage > storage:
+            return i
+    
+    return -1
