238b177d9a0d799aadff990d013f5b4cfa9904f6
Soohyeon Park <sooh21@sookmyung.ac.kr>
2026-06-08 23:53:35 +0900

    #array_and_query_4

---
+++ programmers/level_0/arrary_and_query_4.py
@@ -0,0 +1,11 @@
+# 수열과 구간 쿼리 4
+
+# 3%0 # ZeroDivisionError
+
+def solution(arr, queries):
+    for s, e, k in queries:
+        for i in range(s, e+1):
+            if (i%k == 0):
+                arr[i] += 1
+                
+    return arr
