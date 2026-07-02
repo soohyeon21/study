077ebc1a6621593cb7229c8ea3eaff4a92c6685a
Soohyeon Park <sooh21@sookmyung.ac.kr>
2026-07-02 23:41:41 +0900

    #cut_list

---
+++ programmers/level_0/cut_list.py
@@ -0,0 +1,12 @@
+# 리스트 자르기
+
+def solution(n, slicer, num_list):
+    a, b, c = slicer
+    if (n == 1):
+        return num_list[:b+1]
+    elif (n == 2):
+        return num_list[a:]
+    elif (n == 3):
+        return num_list[a:b+1]
+    elif (n == 4):
+        return num_list[a:b+1:c]
