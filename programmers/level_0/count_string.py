a2d3a8b164eaa178a802b14371ace2fca307d9f8
Soohyeon Park <sooh21@sookmyung.ac.kr>
2026-07-28 23:31:37 +0900

    #count_string

---
+++ programmers/level_0/count_string.py
@@ -0,0 +1,9 @@
+# 문자열이 몇 번 등장하는지 세기
+
+def solution(myString, pat):
+    answer = 0
+    for i in range(len(myString)-len(pat)+1):
+        if (myString[i:i+len(pat)] == pat):
+            answer += 1
+    
+    return answer
