06d0c5a59bb3027636ebc9ed4f785885b287027d
Soohyeon Park <sooh21@sookmyung.ac.kr>
2026-09-12 21:39:36 +0900

    #181838

---
+++ programmers/level_0/181838.py
@@ -0,0 +1,11 @@
+# 181838
+# 날짜 비교하기
+
+def solution(date1, date2):
+    answer = 0
+    for i in range(3):
+        if (date1[i] < date2[i]):
+            return 1
+        elif (date1[i] > date2[i]):
+            return 0
+    return answer
